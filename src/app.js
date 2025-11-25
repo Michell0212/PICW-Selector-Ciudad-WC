ad17b92b618195687d02adddf5c084c118577865
unknown <13649850>
2025-11-25 10:08:46 -0500

    Agrego componente CityItem

--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import { CityApp } from './components/CityApp.js';
 import { CitySelector } from './components/CitySelector.js';
 import { CityInfo } from './components/CityInfo.js';
 import { CityList } from './components/CityList.js';
-
+import { CityItem } from './components/CityItem.js';
